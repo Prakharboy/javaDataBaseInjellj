dfa6d0a661dec1cdc6cbf307c519ebba22503b0d
prakhar bajpai <prakhar.bajpai350@gmail.com>
2019-09-27 10:43:36 +0530

    jdbcTemplate

--- src/main/java/com/stackroute/SqlStatement.java
+++ src/main/java/com/stackroute/SqlStatement.java
@@ -1,69 +1,112 @@
 package com.stackroute;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
-public class SqlStatement extends HttpServlet {
+public class SqlStatement extends HttpServlet implements RowMapper<SqlStatement> {
     private static final long serialVersionUID = 1L;
 
     private String firstName;
     private String LastName;
-    private int id;
-    private int age;
 
-    public static void main(String[] args) throws ServletException, IOException {
-        SqlStatement sqlStatement=new SqlStatement();
-        sqlStatement.doPost();
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    protected void doPost() throws ServletException, IOException
-    {
-        try {
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
 
 
-            Connection con = DataBaseTest.initializeDatabase();
 
-            String sql = new StringBuilder().append("CREATE TABLE REGISTRATIONn").append("(id INTEGER not NULL,").append("first VARCHAR(255),").append("last VARCHAR(255),").append(" age INTEGER,").append(" PRIMARY KEY ( id ));").toString();
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
 
-//            String insertSql=new StringBuilder().append("insert into REGISTRATIONn values").append("(1,").append("'prakhar',").append("'bajpai',").append("22);").toString();
-//
-//            System.out.println(insertSql);
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-//            System.out.println(sql);
-             PreparedStatement statement;
-//            = con
-//                    .prepareStatement(insertSql);
-//
-//            st.executeUpdate();
-
-
-            statement = con.prepareStatement("SELECT * FROM REGISTRATIONn;");
+    private int id;
 
 
-            ResultSet rs = statement.executeQuery();
+    private int age;
 
-            while(rs.next()) {
-                this.id = rs.getInt("id");
-                this.firstName = rs.getString("first");
-                this.LastName = rs.getString("last");
-                this.age = rs.getInt("age");
-            }
 
-            System.out.println(this.age+this.firstName);
+    public static void main(String[] args) throws IOException {
+        JdbcTemplate jdbcTemplatel;
+        DataSource dataSource = null;
 
-            statement.close();
-            con.close();
 
+ApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
+dataSource=(DataSource)context.getBean(DataSource.class);
+        jdbcTemplatel = new JdbcTemplate(dataSource);
+
+
+
+
+      //  System.out.println(jdbcTemplatel.queryForRowSet("select * from REGISTRATIONn;"));
+
+        List<Map<String, Object>> sss=jdbcTemplatel.queryForList("select * from REGISTRATIONn;");
+
+        for(Map<String, Object> row : sss){
+            String id = row.get("id").toString();
+            String name = (String)row.get("first");
+            System.out.println(id + " " + name + " ");
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        SqlStatement sd=new SqlStatement();;
+
+
+
+    }
+
+
+
+    @Override
+    public SqlStatement mapRow(ResultSet resultSet, int i) throws SQLException {
+
+
+        SqlStatement st=new SqlStatement();
+        st.setId(resultSet.getInt("id"));
+        st.setFirstName(resultSet.getString("first"));
+
+        return st;
+
     }
 }
